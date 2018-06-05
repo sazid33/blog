90ce59f3c9dbd1c2b573b7a4d37786dcba2f0439
sazid33 <sazid140233@gmail.com>
2018-06-05 17:08:11 +0600

    table updated

--- database/migrations/2018_06_04_104954_create_users_table.php
+++ database/migrations/2018_06_04_104954_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('user_name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
             
             $table->integer('role_id');
             $table->timestamps();
